d8e5e75adc416f4d8e32bd707cc9aaaaff9411b6
jeffk@contextualmediagroup.com <none@none>
2009-10-18 16:56:09 -0700

    type conversion added for needles table for testing

--- tools/ifcmgtool_compile_from_db.cpp
+++ tools/ifcmgtool_compile_from_db.cpp
@@ -17,6 +17,74 @@ ALL RIGHTS RESERVED.
 #include "soci-postgresql.h"
 #include "ifcmg_tree.hpp"
 
+
+struct db_needle_t
+{
+  db_needle_t() :
+    m_id(0),
+    m_needle(""),
+    m_section(-1),
+    m_autogen(0),
+    m_score(1)
+  {
+    for( int i=0; i<4; ++i )
+    {
+      m_category[i] = -1;
+    }
+  }
+
+  friend
+  std::ostream & operator << ( std::ostream &o, const db_needle_t &v )
+  {
+    o << v.m_id << "," << "\"" << v.m_needle << "\"," << v.m_section << ","
+      << v.m_category[0] << "," << v.m_category[1] << ","
+      << v.m_category[2] << "," << v.m_category[3] << ","
+      << (v.m_autogen ? "true" : "false") << "," << v.m_score;
+      
+    return o;
+  }
+  
+  int64_t m_id;
+  std::string m_needle;
+  int64_t m_section;
+  int64_t m_category[4];
+  int32_t m_autogen;
+  int32_t m_score;
+};
+
+namespace soci
+{
+  template<> struct type_conversion<db_needle_t>
+  {
+    typedef values base_type;
+    static void from_base(values const & v, indicator /* ind */, db_needle_t & n)
+    {
+      n.m_id = v.get<int>("ID");
+      n.m_needle = v.get<std::string>("needle");
+      n.m_section = v.get<int64_t>("section");
+      n.m_category[0] = v.get<int64_t>("category1");
+      n.m_category[1] = v.get<int64_t>("category2",int64_t(-1) );
+      n.m_category[2] = v.get<int64_t>("category3",int64_t(-1) );
+      n.m_category[3] = v.get<int64_t>("category4",int64_t(-1) );
+      n.m_autogen = v.get<int32_t>("autogen",0);
+      n.m_score = v.get<int32_t>("score",1);
+
+    }
+    static void to_base(const db_needle_t & n, values & v, indicator & ind)
+    {
+      v.set("id", n.m_id);
+      v.set("section", n.m_section);
+      v.set("category1", n.m_category[0] );
+      v.set("category2", n.m_category[1] );
+      v.set("category3", n.m_category[2] );
+      v.set("category4", n.m_category[3] );
+      v.set("autogen", n.m_autogen );
+      v.set("score", n.m_score );
+      ind = i_ok;
+    }
+  };
+}
+
 static void usage()
 {
   std::cerr << "usage:\nifcmgtool_compile_from_db user password name host port\n" << std::flush;
@@ -49,8 +117,13 @@ int main( int argc, char **argv )
 
     for( vector<string>::const_iterator i = category_names.begin(); i!=category_names.end(); ++i )
     {
-      cout << *i << "\n";
+      cout << *i << "\n";      
     }
+
+    db_needle_t n;
+    sql << "select (id,needle,section,category1,category2,category3,category4,autogen,score) from needles where id = 12253831", into (n);
+
+    cout << n << "\n";
   }
   catch( std::exception const &e  )
   {
