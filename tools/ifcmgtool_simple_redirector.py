ada1de6ab02b460b50beccdf4089ac68d792353a
jeffk@contextualmediagroup.com <none@none>
2009-12-02 12:58:58 -0800

    more exception handling and logging of known and unknown referers into separate files.

--- tools/ifcmgtool_simple_redirector.py
+++ tools/ifcmgtool_simple_redirector.py
@@ -12,6 +12,7 @@ import urlparse
 
 define("port", default=8080, help="run port", type=int)
 
+
 class FavIcon(tornado.web.RequestHandler):
   def get(self):
     self.set_header("Content-Type","image/vnd.microsoft.icon") 
@@ -45,16 +46,31 @@ class SimpleRedirector(tornado.web.RequestHandler):
     
     r=ifcmgkernel.scan_url(trace,referer_url.netloc, referer_url.geturl())
 
-    result = r[0]
+    result = r[0]-1
     proof = r[1]
     
     fil = "bw"
+    gotit = False
+    logline = referer_url.geturl() + " " + str(result) + "\n"
     if result == 0:
       fil = "gw"
+      self.log_known_good.write(logline)
+      self.log_known_good.flush()
+      gotit = True
     if result == 1:
       fil = "bw"
+      self.log_known_bad.write(logline)
+      self.log_known_bad.flush()
+      gotit = True
     if result == 36:
       fil = "unknown"
+      self.log_unknown.write(logline)
+      self.log_unknown.flush()
+      gotit = True
+
+    if gotit != True:
+      self.log.unknown.write(logline)
+
     params = {
       'ad_type' : ad_type,
       'ad_size' : ad_size,
@@ -71,11 +87,12 @@ class SimpleRedirector(tornado.web.RequestHandler):
       self.write("section = " + section + "\n" )
       self.write("referer was: " + referer + "\n")
       self.write("redirection to : " + redirect_to + "\n")
+      self.write("result is : " + str(result) + "\n" )
       self.write("proof is : \n" + proof + "\n")
     else:
       self.redirect( redirect_to )
 
-def main():
+def main():  
   cmg_home = '/opt/cmg'
   if os.environ.has_key('CMG_HOME'):
     cmg_home = os.environ['CMG_HOME']
@@ -85,19 +102,30 @@ def main():
     os.path.join(precompiled_path,'urls.pre'),
     os.path.join(precompiled_path,'phrases.pre')
     )
+  log_known_good = open("known_good.log","a")
+  log_unknown = open("unknown.log","a")
+  log_known_bad = open("known_bad.log","a")
   
-  tornado.options.parse_command_line()
-  application = tornado.web.Application([
-    (r"/health", HealthCheck ),
-	  (r"/st", SimpleRedirector ),
-    (r"/favicon.ico", FavIcon ),
-    (r"/.*", AutoRedirector ),
-  ])
-  http_server = tornado.httpserver.HTTPServer(application)
-  http_server.listen(options.port)
-  tornado.ioloop.IOLoop.instance().start()
-
-if __name__ == "__main__":
-    main()
+  SimpleRedirector.log_known_good = log_known_good
+  SimpleRedirector.log_unknown = log_unknown
+  SimpleRedirector.log_known_bad = log_known_bad
+  try:
+    tornado.options.parse_command_line()
+    application = tornado.web.Application([
+        (r"/health", HealthCheck ),
+        (r"/st", SimpleRedirector ),
+        (r"/favicon.ico", FavIcon ),
+        (r"/.*", AutoRedirector ),
+        ])
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+  finally:
     ifcmgkernel.shutdown()
-    
+    log_known_bad.close()
+    log_known_good.close()
+    log_unknown.close()
+      
+if __name__ == "__main__":
+  main()
+
