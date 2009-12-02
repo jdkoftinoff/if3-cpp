71518219b94fcd2cc1a22cf6e125f8a6de61b337
jeffk@contextualmediagroup.com <none@none>
2009-12-01 18:36:42 -0800

    More integrations of python with ifcmgkernel, allows proof text report to be returned by scan_url and scan_data. both functions also now take the first parameter as an integer, 0 = no proof needed, 1 = proof needed.

--- python/ifcmgkernel.cpp
+++ python/ifcmgkernel.cpp
@@ -7,10 +7,12 @@
 #include "ifcmg_dynbuf.hpp"
 
 static PyObject *ifcmgkernel_startup(PyObject *self, PyObject *args);
-static PyObject *ifcmgkernel_run_scan(PyObject *self, PyObject *args);
+static PyObject *ifcmgkernel_scan_data(PyObject *self, PyObject *args);
+static PyObject *ifcmgkernel_scan_url(PyObject *self, PyObject *args);
 
 static PyMethodDef IfcmgkernelMethods[] = {
-  {"run_scan", ifcmgkernel_run_scan, METH_VARARGS, "run scan on data"},
+  {"scan_data", ifcmgkernel_scan_data, METH_VARARGS, "run scan on data"},
+  {"scan_url", ifcmgkernel_scan_url, METH_VARARGS, "run scan on url"},
   {"startup", ifcmgkernel_startup, METH_VARARGS, "start up the filter tables"},
   {NULL, NULL, 0, NULL} /* Sentinel */
 };
@@ -25,11 +27,14 @@ initifcmgkernel(void)
 }
 
 static PyObject *
-ifcmgkernel_run_scan(PyObject *self, PyObject *args)
+ifcmgkernel_scan_data(PyObject *self, PyObject *args)
 {
+  int enable_proof_int;
+  bool enable_proof;
+
   const char *hostname;
   size_t hostname_length;
-  
+
   const char *link;
   size_t link_length;
 
@@ -46,9 +51,9 @@ ifcmgkernel_run_scan(PyObject *self, PyObject *args)
 
   int accessed = 0;
   int category = 0;
-  int ad_tags_found = 0;
 
-  if (!PyArg_ParseTuple(args, "s#s#s#s#s#|s",
+  if (!PyArg_ParseTuple(args, "is#s#s#s#s#|s",
+                        &enable_proof_int,
                         &hostname, &hostname_length,
                         &link, &link_length, &text, &text_length,
                         &links, &links_length,
@@ -56,6 +61,8 @@ ifcmgkernel_run_scan(PyObject *self, PyObject *args)
                         categories_enable
                         ))
     return NULL;
+
+  enable_proof = (enable_proof_int!=0);
 
   ifcmg::kernel::categories_enable_t categories_enable_bits;
 
@@ -67,16 +74,71 @@ ifcmgkernel_run_scan(PyObject *self, PyObject *args)
   ifcmg::kernel::full_scan_request_t request;
   ifcmg::kernel::full_scan_results_t results;
 
-  category = kernel->perform_scan(results,request,categories_enable_bits,true);
+  request.hostname( hostname, hostname_length );
+  request.link( link, link_length );
+  request.text( text, text_length );
+  request.links( links, links_length );
+  request.content_links( content_links, content_links_length );
+
+  category = kernel->perform_scan(results,request,categories_enable_bits,enable_proof);
 
   if( text_length + links_length + content_links_length > 1000 )
   {
     accessed=1;
   }
-  
-  std::cerr << results << "\n";
-  
-  return Py_BuildValue("iii", accessed, category+1, ad_tags_found);
+
+  std::stringstream ss;
+  ss << results << "\n";
+
+  return Py_BuildValue("iis#", accessed, category+1, ss.str().c_str(), ss.str().length() );
+}
+
+
+static PyObject *
+ifcmgkernel_scan_url(PyObject *self, PyObject *args)
+{
+  int enable_proof_int;
+  bool enable_proof;
+
+  const char *hostname;
+  size_t hostname_length;
+
+  const char *link;
+  size_t link_length;
+
+  const char *categories_enable = 0;
+
+  int category = 0;
+
+  if (!PyArg_ParseTuple(args, "is#s#|s",
+                        &enable_proof_int,
+                        &hostname, &hostname_length,
+                        &link, &link_length,
+                        categories_enable
+                        ))
+    return NULL;
+
+  enable_proof = (enable_proof_int !=0);
+
+  ifcmg::kernel::categories_enable_t categories_enable_bits;
+
+  if (categories_enable)
+  {
+    categories_enable_bits.set_from_string(std::string(categories_enable));
+  }
+
+  ifcmg::kernel::full_scan_request_t request;
+  ifcmg::kernel::full_scan_results_t results;
+
+  request.hostname( hostname, hostname_length );
+  request.link( link, link_length );
+
+  category = kernel->perform_scan(results,request,categories_enable_bits,enable_proof);
+
+  std::stringstream ss;
+  ss << results << "\n";
+
+  return Py_BuildValue("is#", category+1, ss.str().c_str(), ss.str().length() );
 }
 
 
@@ -88,14 +150,14 @@ ifcmgkernel_startup(PyObject *self, PyObject *args)
   const char *compiled_url_filename;
   const char *compiled_alphanumeric_filename;
 
-  if (!PyArg_ParseTuple(args, "sss", 
+  if (!PyArg_ParseTuple(args, "sss",
                         &compiled_hostname_filename,
                         &compiled_url_filename,
                         &compiled_alphanumeric_filename ) )
   {
     return NULL;
   }
-  
+
   if (kernel != 0)
     delete kernel;
 
--- tools/ifcmgtool_url_query.py
+++ tools/ifcmgtool_url_query.py
@@ -9,9 +9,14 @@ def main():
   if os.environ.has_key('CMG_PREFIX'):
     CMG_PREFIX = os.environ['CMG_PREFIX']
   precompiled_path = CMG_PREFIX + '/share/ifcmgdb-pre'
-  noncompiled_path = CMG_PREFIX + '/share/ifcmgdb-non'
-  ifcmgkernel.startup( precompiled_path, noncompiled_path)
+
+  compiled_hostname_filename = os.path.join( precompiled_path, 'hostnames.pre' )
+  compiled_url_filename = os.path.join( precompiled_path, 'urls.pre' )
+  compiled_alphanumeric_filename = os.path.join( precompiled_path, 'alphanumeric.pre' )
+  ifcmgkernel.startup( compiled_hostname_filename, compiled_url_filename, compiled_alphanumeric_filename )
+  
   print ifcmgkernel.scan_url( sys.argv[1] )
 
 if __name__ == "__main__":
     main()
+
