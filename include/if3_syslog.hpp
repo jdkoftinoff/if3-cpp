762f8e7817f95140cb0527b1bd48706b7af8cf6d
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2010-07-22 13:45:51 -0700

    fixes for vs2010

--- include/if3_syslog.hpp
+++ include/if3_syslog.hpp
@@ -52,7 +52,52 @@ namespace if3
 #  define IF3_LOG_DEBUG(...)
 # endif
 #else
-# define IF3_LOG_DEBUG(...)
+
+  class system_logger_t
+  {
+  public:
+
+    system_logger_t(
+                    string_t identity,
+                    bool to_stderr,
+                    filename_t dest_filename = filename_t("")
+                    );
+    ~system_logger_t();
+
+    void enable_debug( bool f=true )
+    {
+      m_debug_enabled = f;
+    }
+
+    bool is_debug_enabled() const
+    {
+      return m_debug_enabled;
+    }
+
+
+    void log_alert( const char *fmt, ... );
+    void log_critical( const char *fmt, ... );
+    void log_error( const char *fmt, ... );
+    void log_warning( const char *fmt, ... );
+    void log_notice( const char *fmt, ... );
+    void log_info( const char *fmt, ... );
+    void log_debug( const char *fmt, ... );
+
+  private:
+    void do_log_debug( string_t s );
+
+    bool m_debug_enabled;
+
+    FILE * m_local_file_handle;
+  };
+
+  extern system_logger_t *logger;
+# if defined(IF3_CONFIG_DEBUG)
+#  define IF3_LOG_DEBUG(...) logger->log_debug( __VA_ARGS__ )
+# else
+#  define IF3_LOG_DEBUG(...)
+# endif
+
 #endif
 }
 
--- include/if3_world_precompiled.hpp
+++ include/if3_world_precompiled.hpp
@@ -150,5 +150,22 @@ namespace if3
 
 }
 
+
+#ifdef _WIN32
+# if defined(_MSC_VER) || defined(_MSC_EXTENSIONS)
+#  define US_DELTA_EPOCH_IN_MICROSECS  11644473600000000Ui64
+# else
+#  define US_DELTA_EPOCH_IN_MICROSECS  11644473600000000ULL
+# endif
+
+  struct timezone
+  {
+    int tz_minuteswest; /* minutes W of Greenwich */
+    int tz_dsttime; /* type of dst correction */
+  };
+
+  int gettimeofday(struct timeval *tv, struct timezone *tz);
+#endif
+
 #endif
 
